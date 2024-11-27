c14f86c8547609c1e7a4ae61ed95a8b0645fdc91
Vtuber大先生 <apb5377@gmail.com>
2024-11-27 20:45:38 +0900

    [add] daisensei original script

---
+++ dss_blipper/config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+Config.Points = {
+--  --Example Point set
+--  {
+--      blipLabel = "Sample Blip Point",            -- label when you show map
+--      blipSprite = 0,                             -- map icon(default = 0)
+--      blipColor = 5,                              -- map icon color(default = 0)
+--      blipScale = 0.8,                            -- map icon size(default = 1)
+--      point = vector3(-376.78, -146.46, 38.68),   -- map icon position
+--  }
+    {
+        blipLabel = "Sample Blip Point",
+        blipSprite = 0,
+        blipColor = 5,
+        blipScale = 0.8,
+        point = vector3(-376.78, -146.46, 38.68),
+    }
+}
---
+++ dss_blipper/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Daisensei'
+name 'dss_blipper'
+description 'Daisensei\'s set Blip script'
+repository ''
+version '0.0.1'
+
+server_scripts {
+    'server/main.lua',
+}
+
+client_scripts {
+    'client/main.lua',
+}
+
+shared_scripts {
+    'config.lua'
+}
+
+files {
+    -- 'client/*.lua',
+    -- 'web/**/*',
+}
