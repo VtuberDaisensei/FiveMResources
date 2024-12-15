5d2cc59245e0cb3b1025b6e6a68c1ffca7f8a2ad
Vtuber大先生 <apb5377@gmail.com>
2024-12-15 19:19:09 +0900

    [upd] qb-adminmenuからps-adminmenuに変更

--- ps-adminmenu/shared/config.lua
+++ ps-adminmenu/shared/config.lua
@@ -3,7 +3,7 @@ Config = Config or {}
 Config.Fuel = "ps-fuel"        -- "ps-fuel", "LegacyFuel", "ox_fuel"
 Config.ResourcePerms = 'admin' -- permission to control resource(start stop restart)
 Config.ShowCommandsPerms = 'admin' -- permission to show all commands
-Config.RenewedPhone = false    -- if you use qb-phone from renewed. (multijob)
+Config.RenewedPhone = true     -- if you use qb-phone from renewed. (multijob)
 
 -- Key Bindings
 Config.Keybindings = true
